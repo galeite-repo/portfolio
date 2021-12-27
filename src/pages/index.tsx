a9157186a9410173122be6a646deb2ce5d392d46
Gabriel Leite <galeite@gmail.com>
2021-12-27 13:41:38 -0300

    home and expeciences

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,16 @@
-
-
+import Experiences from '../components/Experiences';
+import Header from '../components/Header';
+import HomeHero from '../components/HomeHero';
+import { HomeContainer } from '../styles/HomeStyles';
 
 export default function Home() {
   return (
-    <div>
-
-    </div>
+    <HomeContainer>
+      <Header />
+      <main className="container">
+        <HomeHero />
+        <Experiences />
+      </main>
+    </HomeContainer>
   );
 }
