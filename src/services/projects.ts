1a4bd2d75097fd982a52205edd70a2f256d9dc3c
Gabriel Araujo Leite <galeite@ibm.com>
2022-07-22 09:08:56 -0300

    change revalidate for 5 minutes

--- src/services/projects.ts
+++ src/services/projects.ts
@@ -37,7 +37,8 @@ export default async function ProjectsApi() {
 
   return {
     props: { projects, person, experiences },
-    revalidate: 86400 // 24horas
+    revalidate: 300 // 5min
+    
   };
 }
 
@@ -55,6 +56,6 @@ export async function ProjectById(id: string) {
 
   return {
     props: { project },
-    revalidate: 86400 // 24horas
+    revalidate: 300 // 5min
   };
 }
