a2749b6af70fa53c72c2c7cfcd3a4b34e30a0586
Gabriel Araujo Leite <galeite@ibm.com>
2022-08-09 09:11:52 -0300

    add sendgrid

--- src/pages/api/contact.ts
+++ src/pages/api/contact.ts
@@ -1,38 +1,14 @@
 import nodemailer from 'nodemailer';
-
+import sendgrid from '@sendgrid/mail';
 const email = process.env.MAILADDRESS;
 const emailPass = process.env.MAILPASS;
 
-const transporter = nodemailer.createTransport({
-  host: "smtp.office365.com",
-  secure: false,
-  port: 587,
-  tls: {
-    rejectUnauthorized: false,
-    // ciphers: 'SSLv3'
-  },
-  auth: {
-    user: email,
-    pass: emailPass
-  }
+sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 
-});
- const  mailer = async ({ senderMail, name, text }) => {
-  const from = email;
-  const message = {
-    from,
-    to: `galeite@icloud.com`,
-    subject: `[Portfolio] - Nova mensagem - [${senderMail}]`,
-    text,
-  };
 
-  return await new Promise((resolve, reject) => {
-    transporter.sendMail(message, (error, info) => {
-      error ? reject(error) : resolve(info)
-    }
-    );
-  });
-};
+
+
+
 export default async (req, res) => {
   const { senderMail, name, content } = req.body;
 
@@ -40,6 +16,17 @@ export default async (req, res) => {
     res.status(403).send();
     return;
   }
-  const mailerRes = await mailer({ senderMail, name, text: content });
-  res.send(mailerRes);
+  try {
+    await sendgrid.send({
+      to: 'galeite@icloud.com',
+      from: "galeite@hotmail.com",
+      subject: `[Portfolio News] - [${name}] - ${senderMail}`,
+      text: content
+    });
+  } catch (error) {
+    console.log(error)
+    return res.status(error.statusCode || 500).json({ error: error.message });
+  }
+
+  return res.status(200).json({ error: '' });
 };
